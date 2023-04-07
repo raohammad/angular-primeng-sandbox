ad345193caf460ca0e7dfc9de97a33964a4b7556
Hammad KHAN <raohammad@gmail.com>
2023-04-07 05:03:04 +0200

    code working

--- src/app/demo/splitter-size-demo.ts
+++ src/app/demo/splitter-size-demo.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'splitter-size-demo',
-    templateUrl: './splitter-size-demo.html'
+  selector: "splitter-size-demo",
+  templateUrl: "./splitter-size-demo.html",
 })
 export class SplitterSizeDemo {
-  editorOptions = {theme: 'vs-dark', language: 'javascript', autoLayout: true};
-  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
+  editorOptions = {
+    theme: "vs-dark",
+    language: "javascript",
+    autoLayout: true,
+    automaticLayout: true,
+  };
+  code: string = 'function x() {\nconsole.log("Hello world!");\n}';
 }
